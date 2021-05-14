a4a3b138ff642aeced455b9a1dc0987372d4e16b
Hector <atiliogoulart@gmail.com>
2021-05-14 20:23:02 -0300

    Adding venv to make my life safed

--- config/settings.py
+++ config/settings.py
@@ -4,9 +4,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-=d1%ih!e!22h95o#)$xqb$xo(^mc0u8d-8c6r-9pn215b1o*48'
 
