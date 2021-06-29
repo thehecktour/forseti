571b49b0dd9471f27264ec146fedc54f7586cea9
Atilio <atiliogoulart@gmail.com>
2021-06-29 16:52:50 -0300

    Updating API

--- app/bitcoin/apps.py
+++ app/bitcoin/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BitcoinConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bitcoin'
+    name = 'app.bitcoin'
--- app/settings.py
+++ app/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'bitcoin'
+    'app.bitcoin'
 ]
 
 MIDDLEWARE = [
