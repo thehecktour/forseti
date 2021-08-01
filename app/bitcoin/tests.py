c56f43c1d61569f55998e8533a7aae608333034c
Atilio <atiliogoulart@gmail.com>
2021-08-01 13:58:36 -0300

    Change code

--- app/bitcoin/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-from . import operacao
-
-resultado = operacao.price_bitcoin
-
-if resultado < 30000:
-    print('ERRO')
--- operacao.py
+++ operacao.py
@@ -4,4 +4,6 @@ def price_bitcoin():
     response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
     data = response.json()
     resultado = data["bpi"]["USD"]["rate"]
-    return resultado
+    print(resultado)
+
+price_bitcoin()
