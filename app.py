4106b49193b1d33f2f22d20efaefd86e8b2692ee
Atilio <atiliogoulart@gmail.com>
2021-08-01 14:12:02 -0300

    Project done

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from operacao import resultado
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -6,7 +7,7 @@ app.config['DEBUG'] = True
 
 @app.route('/')
 def bitcoin():
-    return render_template('index.html')
+    return render_template('index.html', resultado=resultado)
 
 if __name__ == '__main__':
     app.run()
--- operacao.py
+++ operacao.py
@@ -1,9 +1,5 @@
 import requests
 
-def price_bitcoin():
-    response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-    data = response.json()
-    resultado = data["bpi"]["USD"]["rate"]
-    print(resultado)
-
-price_bitcoin()
+response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+data = response.json()
+resultado = data["bpi"]["USD"]["rate"]
