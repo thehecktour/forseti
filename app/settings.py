52abafcee205d1d36d770faae85d729b8523e2f5
Hector <atiliogoulart@gmail.com>
2022-02-02 11:56:59 -0300

    Add settings and urls updates for bitcoin app

--- app/settings.py
+++ app/settings.py
@@ -25,6 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'app.bitcoin',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
