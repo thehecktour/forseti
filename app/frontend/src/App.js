a6c34b5a056b4f057bf28455cbbd6ada63093e86
Hector <atiliogoulart@gmail.com>
2022-02-02 12:18:43 -0300

    Firts steps as frontend developer

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import api from "./Api";
 
 function App() {
+  
+  useEffect(() => {
+    api
+      .get("/clickgb/id")
+      .then((response) => setUser(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+
+  const [user, setUser] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Usuário: {user?.name}</p>
     </div>
   );
 }
